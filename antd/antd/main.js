624ce2c3257d62d839f8da3245d8758a4f94d384
ld122481669@126.com <ld122481669@126.com>
2024-07-16 16:55:53 +0800

    add

---
+++ antd/antd/main.js
@@ -0,0 +1,23 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import httpGo from '@/http/http.js'
+export function createApp() {
+  const app = createSSRApp(App)
+  app.config.globalProperties.$http = httpGo;
+  return {
+    app
+  }
+}
+// #endif
---
+++ antd/antd/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<view v-for="(item,index) in list" :key="index">
+			{{item.text}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				list: [],
+			}
+		},
+		onLoad() {
+			this.load();
+		},
+		methods: {
+			async load() {
+				const result=await this.$http.post('https://sqlite001.ld122481669.workers.dev/', `{
+				     "sql": "select * from db_content ;"
+				}`)
+				this.list = result;
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
